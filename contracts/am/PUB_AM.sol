8225299090bd8aa63f703b99f08e7fdf08daaa6d
francesco <francesco@macbookpro-donini.local>
2023-06-14 23:48:42 +0200

    chore: started adding contracts with policies

---
+++ contracts/am/PUB_AM.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.18;
+
+/**
+ * @title PUB AM - Pisa University Brand Attribute Manager
+ * @author Francesco Donini <francesco.donini@phd.unipi.it>
+ * @notice Contains all the information related to PUB 
+ */
+contract PUB_AM {
+
+}
