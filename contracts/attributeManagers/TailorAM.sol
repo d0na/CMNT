8e8d817b7a0a191cef4639242fcf967eb660bf7c
Francesco <francesco.donini@iit.cnr.it>
2023-06-16 09:23:19 +0200

    chore: forgotten folder

---
+++ contracts/attributeManagers/TailorAM.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.18;
+
+/**
+ * @title Tailor AM - Attribute Manager
+ * @author Francesco Donini <francesco.donini@phd.unipi.it>
+ * @notice Contains all the information related to the tailor 
+ */
+contract TailorAM {
+
+}
