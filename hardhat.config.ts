aaef4cc7b986bc8f06c68e4b1bd1a3a526274590
Francesco <francesco.donini@iit.cnr.it>
2023-09-11 10:48:45 +0200

    config: add comment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -31,7 +31,7 @@ module.exports = {
     localhost: {
       url: "http://127.0.0.1:8545"
     },
-    // polygon - if not workss use POLYGONSCAN_API_KEY
+    // polygon - if doesn't works use POLYGONSCAN_API_KEY
     mumbai: {
       url: "https://rpc-mumbai.maticvigil.com",
       accounts: [process.env.PRIVATE_KEY],
